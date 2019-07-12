2fa49cd9f5fda0f5b5def49547d0597767d0bc53
Conduitry <git@chor.date>
2019-07-12 11:43:37 -0400

    add test

---
+++ test/samples/line-endings/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+	rules: {
+		'linebreak-style': ['error', 'windows'],
+	},
+};
---
+++ test/samples/line-endings/Input.svelte
@@ -0,0 +1,9 @@
+<script context='module'>
+	console.log('Hello');
+</script>
+
+<script>
+	console.log('Hello');
+</script>
+
+{foo}
