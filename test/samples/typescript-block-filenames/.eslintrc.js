81ae07db18bfda269431e807825040911d09055c
Simon Holthausen <simon.holthausen@accso.de>
2021-02-09 11:59:47 +0100

    block files test

---
+++ test/samples/typescript-block-filenames/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	extends: ['plugin:@typescript-eslint/recommended'],
+	plugins: ['@typescript-eslint'],
+	overrides: [
+	  {
+		files: ['**/*.svelte/*_template.ts'],
+		rules: {
+		  curly: 'off',
+		},
+	  },
+	  {
+		files: ['**/*.svelte/*_module.ts'],
+		rules: {
+		  'no-undef': 'off',
+		},
+	  },
+	],
+	settings: {
+	  'svelte3/typescript': require('typescript'),
+	  'svelte3/named-blocks': true,
+	},
+	rules: {
+	  curly: 'error',
+	  'no-undef': 'error',
+	},
+  };
+  
