d86cd2434d183f688eedba65cf149dc04c013f18
Conduitry <git@chor.date>
2021-02-15 18:18:34 -0500

    bundle sourcemap-codec

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
+import node_resolve from '@rollup/plugin-node-resolve';
+
 export default {
 	input: 'src/index.js',
 	output: { file: 'index.js', format: 'cjs' },
+	plugins: [ node_resolve() ],
 };
