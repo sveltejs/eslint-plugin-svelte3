f0c6b4c1bd47ae9015ce1e533cde9a375c37cf47
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-02-17 07:05:38 -0700

    Added tests from Dummdidumm

---
+++ test/samples/typescript-bind-reference/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	extends: ['plugin:@typescript-eslint/recommended'],
+	plugins: ['@typescript-eslint'],
+	settings: {
+		'svelte3/typescript': require('typescript'),
+	},
+};
---
+++ test/samples/typescript-imports/input.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+	import type { Type, UnusedType } from './types';
+	import Component from './Component.svelte';
+	import UnusedComponent from './UnusedComponent.svelte';
+	import { Thing1, Thing2, UnusedThing } from './things';
+	const a: Type = new Thing1();
+	a;
+</script>
+
+<div on:click={() => new Thing2()}></div>
+<Component />
