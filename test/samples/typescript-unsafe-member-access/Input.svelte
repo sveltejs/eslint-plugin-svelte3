0edbcbca9f9473351bf8b295c2f9e46bb1e22acf
Simon Holthausen <simon.holthausen@accso.de>
2021-02-18 16:58:02 +0100

    tabs

--- test/samples/typescript-unsafe-member-access/Input.svelte
+++ test/samples/typescript-unsafe-member-access/Input.svelte
@@ -2,30 +2,30 @@
 	const context_safe = [];
 	const context_unsafe: any = null;
 
-    console.log(context_safe.length);
-    console.log(context_unsafe.length);
+	console.log(context_safe.length);
+	console.log(context_unsafe.length);
 </script>
 
 <script lang="ts">
-    import { writable } from 'svelte/store';
-    import { external_safe, external_unsafe } from './external-file';
-    const instance_safe = [];
-    const instance_unsafe: any = null;
-    $: reactive_safe = instance_safe;
-    $: reactive_unsafe = instance_unsafe;
-    const writable_safe = writable([]);
-    const writable_unsafe = writable(null as any);
+	import { writable } from 'svelte/store';
+	import { external_safe, external_unsafe } from './external-file';
+	const instance_safe = [];
+	const instance_unsafe: any = null;
+	$: reactive_safe = instance_safe;
+	$: reactive_unsafe = instance_unsafe;
+	const writable_safe = writable([]);
+	const writable_unsafe = writable(null as any);
 
 	console.log(context_safe.length);
 	console.log(context_unsafe.length);
 	console.log(external_safe.length);
 	console.log(external_unsafe.length);
-    console.log(instance_safe.length);
-    console.log(instance_unsafe.length);
-    // console.log(reactive_safe.length);  TODO, current limitation
-    console.log(reactive_unsafe.length);
-    // console.log($writable_safe.length);  TODO, current limitation
-    console.log($writable_unsafe.length);
+	console.log(instance_safe.length);
+	console.log(instance_unsafe.length);
+	// console.log(reactive_safe.length);  TODO, current limitation
+	console.log(reactive_unsafe.length);
+	// console.log($writable_safe.length);  TODO, current limitation
+	console.log($writable_unsafe.length);
 </script>
 
 {context_safe.length}
