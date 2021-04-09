d655c8066b72515ed2c6881a6a0ffd73bdf90972
j3rem1e <jeremie.brebec@gmail.com>
2021-04-09 13:53:40 +0200

    Fix unused-var with write-only store

---
+++ test/samples/unused-write-only-store/Input.svelte
@@ -0,0 +1,13 @@
+
+<script>
+	import { imported, createStore } from './store.js';
+
+	const writeOnly = createStore();
+	$writeOnly = 99;
+
+	const readOnly = createStore();
+	$readOnly;
+
+	$imported = 'some value';
+</script>
+<div on:click={() => $imported = 'clicked' }/>
