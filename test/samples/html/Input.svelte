82ff79a12fd6d1d45a098b3fb69d572e42581dfa
sdwvit <sdwvit@inet.ua>
2021-06-29 23:36:49 -0400

    update spec

--- test/samples/html/Input.svelte
+++ test/samples/html/Input.svelte
@@ -63,7 +63,7 @@ comment
     {#if 1 === 'individual'}
       <div class="selected creation-individual">
         {#if 1 && 1}
-          <label class="access-token"></label>
+          <div class="access-token"></div>
         {:else}
           {#if 1}
           <p>You haven't connected any accounts yet.</p>
