7096bd209a23ea9caddc03ed1052b7ff660bc2d7
Bob Fanger <bfanger@gmail.com>
2021-11-05 11:12:11 +0100

    feat: Upgraded unittests to ESLint 8.x

--- test/index.js
+++ test/index.js
@@ -2,23 +2,27 @@
 
 process.chdir(__dirname);
 
-const { CLIEngine } = require('eslint');
+const { ESLint } = require('eslint');
 const assert = require('assert');
 const fs = require('fs');
 
-const cli = new CLIEngine({ reportUnusedDisableDirectives: true });
+const cli = new ESLint({ reportUnusedDisableDirectives: "error" });
 
-for (const name of fs.readdirSync('samples')) {
-	if (name[0] !== '.') {
-		console.log(name);
-		if (process.platform === 'win32' && !fs.existsSync(`samples/${name}/preserve_line_endings`)) {
-			fs.writeFileSync(`samples/${name}/Input.svelte`, fs.readFileSync(`samples/${name}/Input.svelte`).toString().replace(/\r/g, ''));
+async function run() {
+	for (const name of fs.readdirSync('samples')) {
+		if (name[0] !== '.') {
+			console.log(name);
+			if (process.platform === 'win32' && !fs.existsSync(`samples/${name}/preserve_line_endings`)) {
+				fs.writeFileSync(`samples/${name}/Input.svelte`, fs.readFileSync(`samples/${name}/Input.svelte`).toString().replace(/\r/g, ''));
+			}
+			const actual_messages = (await cli.lintFiles([`samples/${name}/Input.svelte`]))[0].messages
+			fs.writeFileSync(`samples/${name}/actual.json`, JSON.stringify(actual_messages, null, '\t'));
+			const expected_messages = JSON.parse(fs.readFileSync(`samples/${name}/expected.json`).toString());
+			assert.equal(actual_messages.length, expected_messages.length);
+			assert.deepStrictEqual(actual_messages, actual_messages.map((message, i) => ({ ...message, ...expected_messages[i] })));
+			console.log('passed!\n');
 		}
-		const actual_messages = cli.executeOnFiles([`samples/${name}/Input.svelte`]).results[0].messages;
-		fs.writeFileSync(`samples/${name}/actual.json`, JSON.stringify(actual_messages, null, '\t'));
-		const expected_messages = JSON.parse(fs.readFileSync(`samples/${name}/expected.json`).toString());
-		assert.equal(actual_messages.length, expected_messages.length);
-		assert.deepStrictEqual(actual_messages, actual_messages.map((message, i) => ({ ...message, ...expected_messages[i] })));
-		console.log('passed!\n');
 	}
 }
+
+run()
