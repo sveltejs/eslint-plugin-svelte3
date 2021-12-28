a8bd9c54a44e00a2a44195d85a74236f0e7a5222
Bri <34875062+Monkatraz@users.noreply.github.com>
2021-12-28 06:49:11 -0700

    add test for inline type imports

---
+++ test/samples/typescript-inline-type-import/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	settings: {
+		'svelte3/typescript': require('typescript'),
+	},
+	rules: {
+		'no-unused-vars': 'error',
+	},
+};
