0d27faecddc6d197e877186637ea05ea829bd2fb
Simon H <5968653+dummdidumm@users.noreply.github.com>
2022-01-03 15:34:32 +0100

    [fix] allow $$Props, $$Slots, $$Events usage (#158)
    Fixes #136

--- test/samples/typescript-special-types/Input.svelte
+++ test/samples/typescript-special-types/Input.svelte
@@ -1,6 +1,22 @@
 <script lang='ts'>
 	type T = $$Generic;
 	type U = $$Generic<string>;
+
+	interface $$Slots {
+		default: {
+			foo: string;
+		}
+	}
+
+	type $$Props = {
+		items: T;
+		message: U;
+	}
+
+	interface $$Events {
+		click: string;
+	}
+
 	export let items: T[];
 	export let message: U;
 </script>
