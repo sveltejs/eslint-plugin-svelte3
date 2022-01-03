62569391b09893ecfa9ea2e34a713ea96a1ccba9
Simon Holthausen <simon.holthausen@accso.de>
2022-01-03 14:31:38 +0100

    add failing test

--- test/samples/typescript-special-types/Input.svelte
+++ test/samples/typescript-special-types/Input.svelte
@@ -1,6 +1,22 @@
 <script lang='ts'>
 	type T = $$Generic;
 	type U = $$Generic<string>;
+
+	interfacce $$Slots {
+		default: {
+			foo: string;
+		}
+	}
+
+	type $$Props = {
+		items: T;
+		message: U;
+	}
+
+	interface $$Events {
+		click: string;
+	}
+
 	export let items: T[];
 	export let message: U;
 </script>
